64e8cac9d4e6eef8d7fbe9e931e9c7e5ae66f251
pedro.medeiros <pedro.medeiros@t2atech.com.br>
2025-05-30 14:55:07 -0300

    adicionando rais

--- server/server.js
+++ server/server.js
@@ -5,13 +5,19 @@ import playlistsRoutes from './routes/playlists.js';
 import videosRoutes from './routes/videos.js';
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Rotas
 app.use('/api/playlists', playlistsRoutes);
 app.use('/api/videos', videosRoutes);
 
+// Porta e host para aceitar conexões externas
 const port = 3001;
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+const host = '0.0.0.0';
+
+app.listen(port, host, () => {
+  console.log(`Servidor rodando na porta ${port} e host ${host}`);
 });
